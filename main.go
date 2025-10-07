703ae17af4e5116f83035a2f327ca4eec6bb66af
pfc15 <pedrofcrux@gmail.com>
2025-10-06 23:48:38 -0300

    trocando a rotina go em outro lugar

--- main.go
+++ main.go
@@ -33,11 +33,11 @@ func main() {
 	http.HandleFunc("/youtube/callback", youtubeCallback)
 	http.HandleFunc("/", ola_mundo)
 
-    go subscribe("UCuAXFkgsw1L7xaCfnd5JJOw") // replace with your channel ID
+     // replace with your channel ID
 
     log.Println("Server started on :8002")
     log.Fatal(http.ListenAndServe(":8002", nil))
-	
+	go subscribe("UCuAXFkgsw1L7xaCfnd5JJOw")
 
 	// yt, _ := newYubeClient(API_KEY)
 
